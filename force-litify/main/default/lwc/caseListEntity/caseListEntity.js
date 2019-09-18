8ada5e5ce1a052fb36bdbafe8f12567770939a7c
Zach Caudle <zach.caudle13@gmail.com>
2019-09-18 08:52:41 -0400

    Case Entities - Implemented the concept of case entities through share records

---
+++ force-litify/main/default/lwc/caseListEntity/caseListEntity.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+/**
+ * Container component that loads the Users with share records
+ * for the specific case
+ */
+export default class CaseListEntity extends LightningElement {
+    @api entity;
+
+    @api
+    removeEntity() {
+        // TODO finish this
+    }
+}
+    
